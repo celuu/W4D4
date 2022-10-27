827739272766e974a4bceddc8d8f7976b4ee6edc
celuu <christine.e.luu@gmail.com>
2022-10-27 15:27:29 -0700

    finished stock picker

--- lib/array.rb
+++ lib/array.rb
@@ -45,3 +45,39 @@ class MyTranspose
         @arr.transpose
     end 
 end
+
+class StockPicker
+    attr_accessor :arr, :buy, :sell
+
+    def initialize(arr)
+        @arr = arr
+        @buy = buy
+        @sell = sell
+    end
+    
+    def stock_picker  #[20, 10, 40, 5, 0, 90]
+        largest = @arr[0]
+        smallest = @arr[0]
+        (0...@arr.length).each do |i|
+            if @arr[i] > largest
+                largest = i 
+            elsif @arr[i] < smallest
+                smallest = i 
+            end
+        end
+        [smallest, largest]
+    end
+
+    def self.sell(day)
+        raise "you have nothing to sell" if @buy = day
+        @sell = day
+        @buy = nil
+        
+    end
+
+    def self.buy(day)
+        @buy = day
+    end
+
+
+end
